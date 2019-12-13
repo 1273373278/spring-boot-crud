f5c235dff2f0d4ba0819e1ea3e40fa5e91e7b3fd
xx <1273373278@qq.com>
2019-12-13 15:54:57 +0800

    完成crud

---
+++ src/main/java/com/xx/springboot/config/MyMvcConfig.java
@@ -0,0 +1,7 @@
+package com.xx.springboot.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
+
+}
